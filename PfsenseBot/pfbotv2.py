f91271be086c48a3c425b395281ba5b5100b5f53
Evangelos Baras <baggelisbaras@hotmail.com>
2023-02-26 01:55:46 +0200

    Updated API , Fixed CGnat missfire , Added Dockerfile

--- PfsenseBot/pfbotv2.py
+++ PfsenseBot/pfbotv2.py
@@ -1,100 +1,56 @@
-import nextcord
+import nextcord, time
 from config import config
-from nextcord.ext import commands, tasks
-from PfsenseFauxapi.PfsenseFauxapi import PfsenseFauxapi
+from nextcord.ext import tasks
 from requests import get
-
-
-HOST = config.HOST
-KEY = config.KEY
 currentIP = ""
 
-SECRET = config.SECRET
-client = commands.Bot(command_prefix="$", intents=nextcord.Intents().default())
-client.remove_command("help")
 
+HEADERS = {"accept": "application/json", "Authorization":f"{config.PFSENSE_USER_ID} {config.PFSENSE_API_TOKEN}"}
+intents = nextcord.Intents.default()
+intents.message_content = True
+#client = commands.Bot(command_prefix='!', intents=intents)
+client = nextcord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print("Ive been attached as '%s'" % client.user)
-
-
-@client.event
-async def on_message(message):
-    if client.user.mentioned_in(message):
-        router = PfsenseFauxapi(HOST, KEY, SECRET)
-        gateway = router.gateway_status()
-        gatewayStats = list(gateway["data"]["gateway_status"].values())[0]
-        RTT = float(gatewayStats["delay"].replace("ms", "")) / 1000
-        RTTsd = float(gatewayStats["stddev"].replace("ms", "")) / 1000
-        loss = float(gatewayStats["loss"].replace("%", ""))
-        embed = nextcord.Embed(
-            title="Gateway statistics: ",
-            description="RTT: {}ms\nRTTsd: {}ms\nPacketLoss: {}%".format(
-                RTT * 1000, RTTsd * 1000, loss
-            ),
-        )
-        await message.channel.send(embed=embed)
-
-
-@client.command()
-async def wanIP(context):
-    router = PfsenseFauxapi(HOST, KEY, SECRET)
-    gateway = router.gateway_status()
-    gatewayStats = list(gateway["data"]["gateway_status"].values())[0]
-    IP = gatewayStats["srcip"]
-    await context.channel.send(
-        embed=nextcord.Embed(
-            title="Current pfsense WAN IP address", description=IP, color=0x3730B8
-        )
-    )
-
-
-@client.command()
-async def gateway(context):
-    router = PfsenseFauxapi(HOST, KEY, SECRET)
-    gateway = router.gateway_status()
-    gatewayStats = list(gateway["data"]["gateway_status"].values())[0]
-    RTT = float(gatewayStats["delay"].replace("ms", "")) / 1000
-    RTTsd = float(gatewayStats["stddev"].replace("ms", "")) / 1000
-    loss = float(gatewayStats["loss"].replace("%", ""))
-    embed = nextcord.Embed(
-    title="Gateway statistics: ",
-        description="RTT: {}ms\nRTTsd: {}ms\nPacketLoss: {}%".format(
-            RTT * 1000, RTTsd * 1000, loss
-        ),
-    )
-    await context.channel.send(embed=embed)
-
+    check.start()
+    
+@client.slash_command(name="status", description="Get the current status of pf-sense")
+async def wanIPfunction(interaction: nextcord.Interaction):
+    print("im called finally")
+    pfsense_URL = f"http://{config.HOST}/api/v1/status/interface"
+    pfAPIResponse = get(pfsense_URL, headers=HEADERS, verify=False).json()
+    IP = pfAPIResponse["data"][0]["ipaddr"]
+    pfStatus = pfAPIResponse["data"][0]["status"]
+    wtfHost = get("https://myip.wtf/json").json()["YourFuckingHostname"]
+    pfgatewayStats = get(f"https://{config.HOST}/api/v1/status/gateway", headers=HEADERS, verify=False).json()['data'][0]
+    rtt = pfgatewayStats['delay']
+    rttsd = pfgatewayStats['stddev']
+    loss = pfgatewayStats['loss']
+    embed = nextcord.Embed(title="Pf-sense WAN", description=f"**-Status: **{pfStatus}\n**-IP address:** {IP}\n**-FQDN:** {wtfHost}\n**-RTT**: {rtt} ms\n**-RTTsd: **{rttsd} ms\n**-LOSS :**{loss}%", 
+        color=0x3730b8)
+    await interaction.response.send_message(embed=embed)
 
 @tasks.loop(minutes=10)
 async def check():
-    await client.wait_until_ready()
+    print("PfsenseBot Runing")
     global currentIP
     channel = client.get_channel(config.CHANNEL_ID)
-    router = PfsenseFauxapi(HOST, KEY, SECRET)
-    gateway = router.gateway_status()
-    print("\n GATEWAY STATUS \n\n", router.gateway_status())  # srcip,Delay,loss,status
-    gatewayStats = list(gateway["data"]["gateway_status"].values())[0]
-    IP = gatewayStats["srcip"]
-
-    PublicIP = get("https://api.ipify.org/").text
-    print(IP, PublicIP)
-    if IP != PublicIP:  # CGNAT is not allways 100. but definetly has a dif public IP ...
-        print("CarierGradeNAT")
-        my_embed = nextcord.Embed(title="WARNING", description="CGNAT DETECTED")
-        await channel.send("<@{}>".format(config.ADMIN_ID), embed=my_embed)
+    wtf = get("https://myip.wtf/json").json()
+    PublicIP= wtf["YourFuckingIPAddress"]
+    pfsense_URL = f"http://{config.HOST}/api/v1/status/interface"
+    pfAPIResponse = get(pfsense_URL, headers=HEADERS, verify=False).json()
+    IP = pfAPIResponse["data"][0]["ipaddr"]
+    if (IP != PublicIP):# CGNAT is not allways 100. but definetly has a dif public IP ...
+        print("PANIK")
+        my_embed = nextcord.Embed(title="WARNING", description = "CGNAT DETECTED")
+        await channel.send("<@{}>".format(config.ADMIN_ID),embed=my_embed)
     else:
-        print("publicIP")
-
+        print("KALM")
+        
     if IP != currentIP:
-        ipEmbed = nextcord.Embed(
-            title="WAN IP update, new ip: ", description=IP, color=0x12B912
-        )
-        await channel.send(embed=ipEmbed)
+        ipEmbed = nextcord.Embed(title="WAN IP updates , new ip: ", description = IP ,color=0x12B912)
+        await channel.send(embed=ipEmbed)##Message location. 
         currentIP = IP
-
-
-check.start()
 client.run(config.BOT_TOKEN)
-
